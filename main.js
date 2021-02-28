0fcb6d8e56412c84ddc27b1807a5f2ca4a7b33bb
Rikkarth <rikkarth@users.noreply.github.com>
2021-02-28 21:48:33 +0000

    Finalized Exercise

---
+++ main.js
@@ -0,0 +1,17 @@
+function createPhoneNumber(numbers = []){
+    let first_wave = [];
+    let second_wave = [];
+    let third_wave = [];
+
+    for(i = 0; i <= 2; i++) {
+        first_wave.push(numbers[i]);
+    }
+    for(i = 3; i <= 5; i++) {
+        second_wave.push(numbers[i]);
+    }
+    for(i = 6; i <= 9; i++) {
+        third_wave.push(numbers[i]);
+    }
+    
+    return `(${first_wave.join('')}) ` + `${second_wave.join('')}-` + `${third_wave.join('')}`;
+  }
---
+++ readme.md
@@ -0,0 +1,3 @@
+This is a challenge from Code Wars that I've completed.
+
+Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
